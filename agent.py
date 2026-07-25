5e04250937761b9ff3be413485ea7ac120ca0fdb
Carlos Ramirez <carlos.ramirez@bebolder.co>
2026-07-24 22:51:27 -0500

    ajuste de librarias y de imports

--- agent.py
+++ agent.py
@@ -9,7 +9,7 @@ from typing import Optional
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_classic.agents import AgentExecutor, create_react_agent
 from langchain_core.tools import tool
-from langchain import hub
+import langchainhub as hub
 
 with open("config.json", "r") as f:
     config = json.load(f)
